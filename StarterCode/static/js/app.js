222cc8a46a38749f93b93a1b51fa86335791d698
Kristen-Beer <Krismason56@gmail.com>
2023-10-29 18:39:17 -0700

    bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -12,3 +12,34 @@ function buildMetadata(sample) {
         };
     });
 }
+
+function buildCharts(sample) {
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+        let samples = data.samples;
+        let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+        let result = resultArray[0];
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        //BUBBLE CHART
+        let bubbleLayout = {
+            title: "Bacteria Cultures per Sample",
+            margin: { t:0 },
+            hovermode: "closest",
+            xaxis: { title: "OTU ID" },
+            margin: { t: 30}
+        };
+        let bubbledata = [
+            {
+                x: otu_ids,
+                y: sample_values,
+                mode: "markers",
+                marker: {
+                    size: sample_values,
+                    color: otu_ids,
+                    colorscale: "Earth"
+                }
+            }
+        ];
+}
